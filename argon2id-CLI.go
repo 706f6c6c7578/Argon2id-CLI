1caceb094319caf7cccb2faaa95c3ed523123fdc
Stefan Claas <stefan.claas@mail.ru>
2024-06-21 15:45:33 +0200

    Add files via upload

---
+++ argon2id-CLI.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"golang.org/x/crypto/argon2"
+	"encoding/hex"
+	"os"
+)
+
+func main() {
+	// Define command-line flags
+	passwordFlag := flag.String("p", "", "Password for Argon2id hashing.")
+	saltFlag := flag.String("s", "", "Salt for Argon2id hashing.")
+
+	// Set up a usage message
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	// Parse the command-line flags
+	flag.Parse()
+
+	// Check for required flags
+	if *passwordFlag == "" || *saltFlag == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Generate the key with Argon2id
+	key := argon2.IDKey([]byte(*passwordFlag), []byte(*saltFlag), 1, 64*1024, 4, 32)
+	result := hex.EncodeToString(key)
+
+	// Output the resulting key
+	fmt.Println(result)
+}
+
+
